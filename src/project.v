f61ebef918d090483e6a3ae646ddc61e9f214837
Claudio Talarico <talarico@gonzaga.edu>
2024-11-08 13:27:54 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
+ * Claudio Talarico
+ * 4-bit up/down binary counter with enable
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_claudiotalarico_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,21 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+ // instantiate top level 
+    tiny u1(.clk(clk),
+            .rst_n(rst_n),
+            .test(ui_in[0]),
+            .ud(ui_in[1]),
+            .en(ui_in[2]),
+            .cnt(uo_out[3:0])
+ );  
+  
+ // All output pins must be assigned. If not used, assign to 0.
+ assign uo_out[7:4]  = 0;  // Example: ou_out is the sum of ui_in and uio_in
+ assign uio_out = 0;
+ assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+// List all unused inputs to prevent warnings
+wire _unused = &{ena , uio_in, ui_in[7:3],1'b0};
 
 endmodule
