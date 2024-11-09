7b92b5825dc5cd9d000a8116c8f3821033d535aa
Claudio Talarico <talarico@gonzaga.edu>
2024-11-08 17:27:39 -0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,30 +11,30 @@ async def test_project(dut):
       
       dut._log.info("Start")
 
-    # Set the clock period to 20 ns (50 MHz)
-    clock = Clock(dut.clk, 20, units="ns")
-    cocotb.start_soon(clock.start())
+      # Set the clock period to 20 ns (50 MHz)
+      clock = Clock(dut.clk, 20, units="ns")
+      cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 20)
-    dut.rst_n.value = 1
+      # Reset
+      dut._log.info("Reset")
+      dut.ena.value = 1
+      dut.ui_in.value = 0
+      dut.uio_in.value = 0
+      dut.rst_n.value = 0
+      await ClockCycles(dut.clk, 20)
+      dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+      dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value[0] = 1'b1; // test (test mode)
-    dut.ui_in.value[1] = 1'b1; // ud   (up/down count)
-    dut.ui_in.value[2] = 1'b1; // en   (enable count)
-    dut.ui_in.value[7:3] = 0;  // don't leave unused inputs floating
-    dut.uio_in.value = 0
+      # Set the input values you want to test
+      dut.ui_in.value[0] = 1'b1; // test (test mode)
+      dut.ui_in.value[1] = 1'b1; // ud   (up/down count)
+      dut.ui_in.value[2] = 1'b1; // en   (enable count)
+      ##dut.ui_in.value[7:3] = 0;  // don't leave unused inputs floating
+      ##dut.uio_in.value = 0
 
-    # Wait for 100 clock cycle to see the output values
-    await ClockCycles(dut.clk, 100)
+      # Wait for 100 clock cycle to see the output values
+      await ClockCycles(dut.clk, 100)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
